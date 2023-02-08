84df768c2cd5bbd9a529873d3ffefab5e88db0d2
d2gher <d2gher@gmail.com>
2023-02-08 18:30:46 +0200

    Refactor code

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,38 +1,32 @@
 # frozen_string_literal: false
 require 'io/console'
 
-$stdout.clear_screen # or STDOUT.clear_screen
+def clear_console
+  $stdout.clear_screen # or STDOUT.clear_screen
+end
 
 # Create a player class
 class Player
-  @@player_count = 0
+  @@count = 0
   attr_accessor :name, :score, :symbol
 
-  def initialize(name, symbol)
+  def initialize(name)
     @name = name.capitalize
-    @symbol = symbol[0].capitalize
+    @symbol = @@count == 1 ? 'X' : 'O'
     @score = 0
-    @@player_count += 1
-
-    if @symbol.to_i.is_a? Integer
-      (@@player_count == 1)? @symbol = 'X': @symbol = 'O'
-    end
+    @@count += 1
   end
 end
 
 def create_player
-  puts "Player #{Player.class_variable_get(:@@player_count) + 1} name:"
+  puts "Player #{Player.class_variable_get(:@@count) + 1} name:"
   player_name = gets.chomp
-
-  puts 'Pick a symbol:'
-  player_symbol = gets.chomp
-
-  Player.new(player_name, player_symbol)
+  Player.new(player_name)
 end
 
 def print_board(board, player1, player2)
-  $stdout.clear_screen # or STDOUT.clear_screen
-  print "
+  clear_console
+  puts "
   |=============================|
   |======== TIC-TAC-TOE ========|
   |=============================|
@@ -50,59 +44,75 @@ def get_player_move(player, board, board_size)
   while move < 1 || move > board_size**2
     puts "\n #{player.name}'s turn! pick a number btween 1 and #{board_size**2}"
     move = gets.chomp.to_i
+    # Make sure this postion hasn't been played
     move = 0 unless board[move - 1].is_a? Integer
   end
   move - 1
 end
 
-def check_if_winner(board, board_size)
-  # Horizontal check
-  h = 0
-  while h < board.length
+def horizontal_win?(board, board_size)
+  row_start = 0
+  while row_start < board.length
     winner = true
-    (board_size - 1).times do |i|
-      current_pos = h + i
+    current_pos = row_start
+    (board_size - 1).times do
       next_pos = current_pos + 1
       winner = false unless board[current_pos] == board[next_pos]
+      current_pos = next_pos
     end
     return true if winner
-    h += board_size
+    row_start += board_size
   end
+end
 
-  # Vertical check
-  v = 0
-  while v < board_size
+def vertical_win?(board, board_size)
+  column_start = 0
+  while column_start < board_size
     winner = true
-    current_pos = v
-    (board_size - 1).times do |i|
+    current_pos = column_start
+    (board_size - 1).times do
       next_pos = current_pos + board_size
       winner = false unless board[current_pos] == board[next_pos]
       current_pos = next_pos
     end
     return true if winner
-    v += 1
-  end
 
-  # Cross check
+    column_start += 1
+  end
+end
+
+def left_cross_win?(board, board_size)
   winner = true
   current_pos = 0
+
   (board_size - 1).times do
     next_pos = current_pos + board_size + 1
     winner = false unless board[current_pos] == board[next_pos]
     current_pos = next_pos
   end
   return true if winner
-  
+end
+
+def right_cross_win?(board, board_size)
   winner = true
   current_pos = board_size - 1
   (board_size - 1).times do
-  next_pos = current_pos + board_size - 1
-  winner = false unless board[current_pos] == board[next_pos]
-  current_pos = next_pos
+    next_pos = current_pos + board_size - 1
+    winner = false unless board[current_pos] == board[next_pos]
+    current_pos = next_pos
   end
   return true if winner
+end
 
-  return false
+def check_if_winner?(board, board_size)
+  return true if horizontal_win?(board, board_size)
+  return true if vertical_win?(board, board_size)
+  return true if left_cross_win?(board, board_size)
+  return true if right_cross_win?(board, board_size)
+end
+
+def draw?(board)
+  board.all? { |e| e.is_a? String }
 end
 
 def print_winner(message)
@@ -113,17 +123,13 @@ def print_winner(message)
   "
 end
 
-def draw?(board)
-  board.all? {|e| e.is_a? String }
-end
-
 def play_a_round(board, board_size, player1, player2)
   print_board(board, player1, player2)
-  while true
+  loop do
     move = get_player_move(player1, board, board_size)
     board[move] = player1.symbol
     print_board(board, player1, player2)
-    if check_if_winner(board, board_size)
+    if check_if_winner?(board, board_size)
       print_winner("#{player1.name} has won!")
       player1.score += 1
       break
@@ -132,10 +138,11 @@ def play_a_round(board, board_size, player1, player2)
       print_winner("It's a draw!")
       break
     end
+
     move = get_player_move(player2, board, board_size)
     board[move] = player2.symbol
     print_board(board, player1, player2)
-    if check_if_winner(board, board_size)
+    if check_if_winner?(board, board_size)
       print_winner("#{player2.name} has won!")
       player2.score += 1
       break
@@ -155,10 +162,11 @@ board = (1..(board_size * board_size)).to_a
 
 play_a_round(board, board_size, player1, player2)
 
-while true
-  puts "\n Wanna play another round? press 'Y'"
-  prompt = STDIN.gets.chomp
-  break unless prompt == "y" || prompt == "Y"
+loop do
+  puts "\n Wanna play another round? Enter 'Y'"
+  prompt = $stdin.gets.chomp
+  break unless %w[y Y].include?(prompt)
+
   board = (1..(board_size * board_size)).to_a
   play_a_round(board, board_size, player1, player2)
 end
